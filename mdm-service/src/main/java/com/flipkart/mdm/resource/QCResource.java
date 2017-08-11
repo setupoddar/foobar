241e43c9a65a7b09f8fca1dcf1c185f5db26a3a1
setu.poddar <setu.poddar@flipkart.com>
2017-08-11 14:58:35 +0530

    AD

--- mdm-service/src/main/java/com/flipkart/mdm/resource/QCResource.java
+++ mdm-service/src/main/java/com/flipkart/mdm/resource/QCResource.java
@@ -2,6 +2,7 @@ package com.flipkart.mdm.resource;
 
 import com.codahale.metrics.annotation.Timed;
 import com.flipkart.mdm.Utils;
+import com.flipkart.mdm.client.ZHttpClient;
 import com.flipkart.mdm.dal.dao.QCFSNDAO;
 import com.flipkart.mdm.dal.dao.TrendDAO;
 import com.flipkart.mdm.dal.exception.DBException;
@@ -10,6 +11,7 @@ import com.flipkart.mdm.model.Trend;
 import com.flipkart.mdm.model.enums.TaskStatus;
 import com.flipkart.mdm.resource.exception.SystemResourceException;
 import com.flipkart.mdm.resource.request.TaskCompleteRequest;
+import com.flipkart.mdm.resource.response.SearchResult;
 import com.google.inject.Inject;
 import io.dropwizard.hibernate.UnitOfWork;
 import lombok.extern.slf4j.Slf4j;
@@ -17,6 +19,9 @@ import lombok.extern.slf4j.Slf4j;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
 
 /**
  * Created by setu.poddar on 10/08/17.
@@ -28,11 +33,13 @@ public class QCResource {
 
     private QCFSNDAO qcfsnDAO;
     private TrendDAO trendDAO;
+    private ZHttpClient zHttpClient;
 
     @Inject
-    public QCResource(QCFSNDAO qcfsnDAO,TrendDAO trendDAO) {
+    public QCResource(QCFSNDAO qcfsnDAO,TrendDAO trendDAO, ZHttpClient zHttpClient) {
         this.qcfsnDAO = qcfsnDAO;
         this.trendDAO = trendDAO;
+        this.zHttpClient= zHttpClient;
     }
 
     @POST
@@ -73,9 +80,19 @@ public class QCResource {
     @UnitOfWork
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response get(@PathParam("id") String id) {
+    public Response getImages(@PathParam("id") String id) {
         try {
-            return Response.ok(qcfsnDAO.findById(id)).build();
+            QCFSN qcfsn = qcfsnDAO.findById(id);
+
+            SearchResult searchResult = new SearchResult();
+            List<String> productIds = Arrays.asList(qcfsn.getFsns().split(","));
+            searchResult.setStart(0);
+            searchResult.setTotalCount(productIds.size());
+            searchResult.setCount(productIds.size());
+            for(String pr : productIds){
+                searchResult.getProductIds().put(pr, new HashSet<String>());
+            }
+            return Response.ok(zHttpClient.getImageUrls(searchResult)).build();
         } catch (DBException e) {
             throw new SystemResourceException(500,e.getMessage());
         }
